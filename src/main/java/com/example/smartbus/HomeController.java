6359e665dba1a91dba288f9d751f669cb89da96d
gowthamitummala-2005 <23eg110c61@anurag.edu.in>
2026-04-03 22:31:57 +0530

    add home controller

---
+++ src/main/java/com/example/smartbus/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.smartbus;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Smart Bus Backend Running";
+    }
+}
